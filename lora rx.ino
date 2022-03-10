a9c6b2cd2f131c280c3091da93dfba81195ea107
NickTheWhale <51139062+NickTheWhale@users.noreply.github.com>
2022-03-09 21:05:21 -0600

    more oled stuff

--- lora rx.ino
+++ lora rx.ino
@@ -18,6 +18,7 @@
 #include <stdlib.h>
 #include <LoRa.h>
 #include <string>
+#include <iostream>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -27,6 +28,7 @@
 #define OLED_RST 16
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define XOFFSET 6
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 
@@ -35,12 +37,13 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 #define loraDIO0 26
 
 #define spreadingFactor 12
-#define signalBandwidth 62.5E3
+#define signalBandwidth 125E3
 #define frequency 915E6
 #define codingRateDenominator 5
 #define preambleLength 8
 #define syncWord 0x12
 #define txPower 20
+#define CRC true
 
 #define TRIGGER_PIN 0
 
@@ -138,30 +141,95 @@ void setup() {
     display.setCursor(0, 0);
     display.print("LoRa init success");
     display.display();
+    delay(2000);
   }
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
   Serial.println();
   LoRa.setFrequency(frequency);
-  Serial.print("Frequency:                 "); Serial.println(frequency); Serial.println();
+  Serial.print("Frequency:                 ");   Serial.println(frequency);                    Serial.println();
+  display.setCursor(0, 0);                       display.print("Frequency:");                   
+  display.setCursor(SCREEN_WIDTH - (String(frequency / 1000000.0).length() * XOFFSET), 0);     display.print(frequency / 1000000.0);
+
   LoRa.setSpreadingFactor(spreadingFactor);
-  Serial.print("Spreading Factor:          "); Serial.println(spreadingFactor); Serial.println();
+  Serial.print("Spreading Factor:          ");   Serial.println(spreadingFactor);              Serial.println();
+
+  display.setCursor(0, 8);                       display.print("Spread: ");
+  display.setCursor(SCREEN_WIDTH - (String(spreadingFactor).length() * XOFFSET), 8);           display.print(spreadingFactor);
+
   LoRa.setSignalBandwidth(signalBandwidth);
-  Serial.print("Signal Bandwidth:          "); Serial.println(signalBandwidth); Serial.println();
+  Serial.print("Signal Bandwidth:          ");   Serial.println(signalBandwidth);              Serial.println();
+
+  display.setCursor(0, 16);                      display.print("Bandwidth: ");
+  display.setCursor(SCREEN_WIDTH - (String(signalBandwidth / 1000.0).length() * XOFFSET), 16); display.print(signalBandwidth / 1000.0);
+
   LoRa.setCodingRate4(codingRateDenominator);
-  Serial.print("Coding Rate (denominator): "); Serial.println(codingRateDenominator); Serial.println();
+  Serial.print("Coding Rate (denominator): ");   Serial.println(codingRateDenominator);        Serial.println();
+
+  display.setCursor(0, 24);                      display.print("Coding Rate: ");
+  display.setCursor(SCREEN_WIDTH - (String(codingRateDenominator).length() * XOFFSET), 24);    display.print(codingRateDenominator);
+
   LoRa.setPreambleLength(preambleLength);
-  Serial.print("Preamble Length:           "); Serial.println(preambleLength); Serial.println();
+  Serial.print("Preamble Length:           ");   Serial.println(preambleLength);               Serial.println();
+
+  display.setCursor(0, 32);                      display.print("Preamble len: ");
+  display.setCursor(SCREEN_WIDTH - (String(preambleLength).length() * XOFFSET), 32);           display.print(preambleLength);
+
   LoRa.setSyncWord(syncWord);
-  Serial.print("Sync word:                 "); Serial.println(syncWord); Serial.println();
+  Serial.print("Sync Word:                 ");   Serial.println(syncWord);                     Serial.println();
+
+  display.setCursor(0, 40);                      display.print("Sync Word: ");
+  display.setCursor(SCREEN_WIDTH - (String(syncWord).length() * XOFFSET), 40);                 display.print(syncWord);
+
   LoRa.setTxPower(txPower);
-  Serial.print("Tx Power                   "); Serial.println(txPower); Serial.println();
+  Serial.print("Tx Power                   ");   Serial.println(txPower);                      Serial.println();
+
+  display.setCursor(0, 48);                      display.print("Tx Power: ");
+  display.setCursor(SCREEN_WIDTH - (String(txPower).length() * XOFFSET), 48);                  display.print(txPower);
+
+  if (CRC) {
+    LoRa.enableCrc();
+  }
+  else {
+    LoRa.disableCrc();
+  }
+  Serial.print("CRC                        "); if (CRC) {
+    Serial.println("enabled");
+
+    display.setCursor(0, 56); display.print("CRC: ");
+    display.setCursor(SCREEN_WIDTH - (String("enabled").length() * XOFFSET), 56); display.print("enabled");
+  } else {
+    Serial.println("disabled");
+
+    display.setCursor(0, 56); display.print("CRC: ");
+    display.setCursor(SCREEN_WIDTH - (String("disabled").length() * XOFFSET), 56); display.print("disabled");
+  }
+  display.display();
+  int cnt = 0;
+  bool pass = false;
+  while (!pass && cnt < 10000) {
+    pass = !digitalRead(TRIGGER_PIN);
+    delay(1);
+    cnt++;
+  }
   LoRa.receive();
-  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
   Serial.println();
-  Serial.println("Setup Finished");
+  Serial.println("Setup Complete");
   Serial.println();
   Serial.println("Trying preset WiFi credentials");
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0, 0);  display.print("Setup Complete");
+  delay(1000);
+  display.setCursor(0, 16); display.print("Trying preset WiFi    credentials");
+  display.display();
+  display.setCursor(0, 32);
 
   int count = 0;
+  int blink = 0;
   int ssidSize = *(&ssid + 1) - ssid;
   int passSize = *(&password + 1) - password;
   for (int i = 0; i < ssidSize; i++) {
@@ -170,6 +238,21 @@ void setup() {
         WiFi.begin(ssid[i], password[j]);
         while (WiFi.status() != WL_CONNECTED && count < 20) {
           Serial.print(".");
+          if (blink < 3) {
+            blink += 1;
+            display.print(".");
+            display.display();
+          }
+          else {
+            display.clearDisplay();
+            display.setTextColor(WHITE);
+            display.setTextSize(1);
+            display.setCursor(0, 0);  display.print("Setup Complete");
+            display.setCursor(0, 16); display.print("Trying preset WiFi    credentials");
+            display.display();
+            display.setCursor(0, 32);
+            blink = 0;
+          }
           count++;
           delay(500);
         }
@@ -183,21 +266,19 @@ void setup() {
   }
   if (WiFi.status() == WL_CONNECTED) {
     currentSSID = WiFi.SSID();
-    Serial.print("connected to ");
+    Serial.print("Connected to ");
     Serial.println(currentSSID);
-    display.clearDisplay();
-    display.setTextColor(WHITE);
-    display.setTextSize(1);
-    display.setCursor(0, 0);
-    display.print("connected to: ");
-    display.setCursor(0, 8);
-    display.print(currentSSID);
+    display.clearDisplay();    display.setTextColor(WHITE);     display.setTextSize(1);
+    display.setCursor(0, 0);   display.print("Setup Complete");
+    display.setCursor(0, 16);  display.print("Trying preset WiFi    credentials");
+    display.setCursor(0, 40);  display.print("Connected to: ");
+    display.setCursor(0, 48);  display.print(currentSSID);
     display.display();
     connection = true;
     setupFlag = true;
   }
   else {
-    Serial.println("preset credentials failed");
+    Serial.println("Preset Credentials Failed");
   }
 }
 
@@ -235,7 +316,7 @@ void loop() {
   if (packetSize)
   {
     // received a packet
-    Serial.println("Received packet '");
+    Serial.println("Received packet ");
 
     // read packet
     while (LoRa.available())
@@ -279,15 +360,13 @@ void loop() {
 
 void sendPush(String message) {
   client.setCACert(test_root_ca);
-  //client.setCertificate(test_client_key); // for client verification
-  //client.setPrivateKey(test_client_cert);  // for client verification
-
   Serial.println("\nStarting connection to server...");
+  Serial.println();
   if (!client.connect(server, 443))
     Serial.println("Connection failed!");
   else {
     Serial.println("Connected to server!");
-
+    Serial.println();
     //String content = "url=" + url + "&token=a57xzp7f57badgc3zyan6rvum6qzzb&user=utdnuh4tt1hd4hutfmk7q95uyoxpdm&device=iphone&title=PushNotfier&message=" + message;
     String content = "token=a57xzp7f57badgc3zyan6rvum6qzzb&user=utdnuh4tt1hd4hutfmk7q95uyoxpdm&device=iphone&title=LoRa&message=" + message;
     // Make a HTTP request:
@@ -304,6 +383,7 @@ void sendPush(String message) {
       String line = client.readStringUntil('\n');
       if (line == "\r") {
         Serial.println("headers received");
+        Serial.println();
         break;
       }
     }
@@ -313,6 +393,7 @@ void sendPush(String message) {
       char c = client.read();
       Serial.write(c);
     }
+    Serial.println();
     client.stop();
   }
 }
